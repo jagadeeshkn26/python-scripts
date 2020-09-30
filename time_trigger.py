d5397563119817af60bd4293ffcb2378cd1fe343
jagadeeshkn26 <30287525+jagadeeshkn26@users.noreply.github.com>
2020-09-30 10:30:09 +0530

    Update time_trigger.py

--- time_trigger.py
+++ time_trigger.py
@@ -18,6 +18,6 @@ print("Current Time: ", current_time)
 # git commit -m "Your Message"
 # git push
 # --------------------------------------------------------------------------------
-quote = "When someone shares something of value with you that benefits you, then you are morally obligated to share it with others!"
+quote = "When someone shares something of value with you that benefits you, then you are morally obligated to share it with others! KN Jagadeesh"
 print(quote)
 # --------------------------------------------------------------------------------
